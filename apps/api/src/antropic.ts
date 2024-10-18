e355bf541908b84c588f31e6a21eb16a4465edce
aymene01 <aymene.bousbia@veepee.com>
2024-10-18 20:25:51 +0200

    chore: add vitest configuration and enhance scripts

---
+++ apps/api/src/antropic.ts
@@ -0,0 +1,29 @@
+import Anthropic from "@anthropic-ai/sdk";
+
+const initializeAnthropic = () => {
+  return new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+  });
+};
+
+export const anthropicClient = initializeAnthropic();
+
+type SendMessage = {
+  message: string;
+  model?: string;
+  max_tokens?: number;
+};
+
+export const sendMessage = async ({
+  message,
+  max_tokens = 1024,
+  model = "claude-3-5-sonnet-20240620",
+}: SendMessage) => {
+  const msg = await anthropicClient.messages.create({
+    model,
+    max_tokens,
+    messages: [{ role: "user", content: message }],
+  });
+
+  return msg;
+};
