17e63f554ed9265b9c8d15ef16cd3fdfa12f04aa
Chahine-tech <chahine.benlahcen@gmail.com>
2024-10-18 14:55:58 +0200

    feat: initialize Anthropic sdk in API

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,23 @@
+import Anthropic from "@anthropic-ai/sdk";
 import express from "express";
 
 const app = express();
+
+async function initializeAnthropic() {
+  const anthropic = new Anthropic({
+    apiKey: "my_api_key",
+  });
+
+  const msg = await anthropic.messages.create({
+    model: "claude-3-5-sonnet-20240620",
+    max_tokens: 1024,
+    messages: [{ role: "user", content: "Hello, Claude" }],
+  });
+
+  console.log(msg);
+}
+
+initializeAnthropic();
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
